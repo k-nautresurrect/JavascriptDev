eb5a1959e6d32394426786c116a4a07ac4ff4550
k-nautresurrect <kushignite@gmail.com>
2022-02-06 18:37:24 +0530

    Let variable in js

--- js-begin/modules/first-js/script.js
+++ js-begin/modules/first-js/script.js
@@ -1,1 +1,24 @@
-console.log("hello World !");
+let afaviouriteNumber = 4_000;
+
+console.log(afaviouriteNumber);
+
+afaviouriteNumber = 5_000;
+
+console.log(afaviouriteNumber);
+
+// exercise
+
+// let a,b,c = 1,2,3; // not possible
+// let a,  b,  c = 1; //  not possible only c will get 1 as a value
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = 3;
+let d = a; // here d will be 3 only will not change to 5
+a = 5;
+console.log(a);
+console.log(d);
