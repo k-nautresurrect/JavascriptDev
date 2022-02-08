1b8376ccac55fabf7f4bedf740d99c74592d56b8
k-nautresurrect <kushignite@gmail.com>
2022-02-08 23:16:58 +0530

    Relational Operators

--- js-begin/modules/first-js/script.js
+++ js-begin/modules/first-js/script.js
@@ -1,5 +1,26 @@
-let nothing = null;
-let de = undefined;
-console.log(Object(nothing));
-console.log(Object(de));
-console.log(nothing);
+let a = 8;
+let b = 9;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b); // a < b || a == b
+console.log(a >= b); // a > b || a == b
+console.log(a ^ b); // bitwise works just fine
+
+console.log("\n");
+
+let str = "brad";
+let str2 = "bradane";
+
+console.log(str == str2);
+console.log(str < str2);
+console.log(str > str2);
+console.log(str >= str2);
+console.log(str <= str2);
+console.log(str != str2);
+// ambigous statement however return 0 on every bitwise operation
+console.log(str ^ str2);
+
+console.log(console.log.toString());
