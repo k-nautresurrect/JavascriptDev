c5d5cadbf170a60f9de8b4205987eb589ceb90c7
k-nautresurrect <kushignite@gmail.com>
2022-02-10 13:39:08 +0530

    Scoping in js

--- js-begin/modules/first-js/script.js
+++ js-begin/modules/first-js/script.js
@@ -1,21 +1,79 @@
-// Stacktrace and call stack by chrome dev
+// Scoping
 
-function doThing(a, b, name) {
-  print(sum(a, b));
-  print(editedName(name));
+// Global Scope
+let a = 3;
+
+// block scope 1
+{
+  let b = 4;
+  // block scope 2
+  {
+    let c = 5;
+    console.log(c);
+    console.log(b);
+    console.log(a);
+  }
 }
 
-function print(variable) {
-  console.log(variable);
+// labels in js
+first: {
+  let i = 0;
+  second: {
+    if (i === 0) {
+      break second;
+    }
+    console.log(i);
+  }
+  while (true) {
+    if (i > 10) {
+      break first;
+    }
+    console.log(i);
+    i++;
+  }
 }
 
-function editedName(name) {
-  // adhoaido; // Stacktrace throws error however a breakpoint can be defined.
-  return "hello " + name;
+function scope() {
+  let d = 9;
+  console.log(a);
 }
 
-function sum(a, b) {
-  return a + b;
+scope();
+
+// to access th other scope variable
+/**
+ * -> either pass the value from one function to other (prefered)
+ * -> make a global object with the variable names
+ * -> use global variable directoly
+ */
+
+// using object
+let scriptObj = {};
+
+scope: {
+  scriptObj.x = 90;
 }
 
-doThing(3, 7, "kushagra");
+function scopeFun() {
+  scriptObj.x = 20;
+}
+
+console.log(scriptObj.x);
+scopeFun();
+console.log(scriptObj.x);
+
+// passing params
+
+function init() {
+  let a = 40;
+  let b = 89;
+  let d = 90;
+  setup(a, b, d);
+}
+
+function setup(...args) {
+  console.log(args[0]);
+  console.log(args[1]);
+}
+
+init();
