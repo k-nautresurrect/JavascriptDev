319c454f59c821b971ddd02a5b0fe51185e23624
k-nautresurrect <kushignite@gmail.com>
2022-02-10 16:05:29 +0530

    Closures in js

--- js-begin/modules/first-js/script.js
+++ js-begin/modules/first-js/script.js
@@ -1,79 +1,27 @@
-// Scoping
+// Closures
+let a = 5;
 
-// Global Scope
-let a = 3;
-
-// block scope 1
-{
-  let b = 4;
-  // block scope 2
-  {
-    let c = 5;
-    console.log(c);
-    console.log(b);
-    console.log(a);
-  }
+function print() {
+  console.log(a); // the latest value of a
 }
 
-// labels in js
-first: {
-  let i = 0;
-  second: {
-    if (i === 0) {
-      break second;
-    }
-    console.log(i);
-  }
-  while (true) {
-    if (i > 10) {
-      break first;
-    }
-    console.log(i);
-    i++;
-  }
-}
+a = 4;
+print();
 
-function scope() {
-  let d = 9;
-  console.log(a);
-}
-
-scope();
-
-// to access th other scope variable
 /**
- * -> either pass the value from one function to other (prefered)
- * -> make a global object with the variable names
- * -> use global variable directoly
+ * closures
+ * accessing something outside their own scope
+ *
  */
 
-// using object
-let scriptObj = {};
-
-scope: {
-  scriptObj.x = 90;
+function closure(variable) {
+  let c = "instring";
+  return function func(variable2) {
+    console.log(variable);
+    console.log(variable2);
+    console.log(c);
+  };
 }
 
-function scopeFun() {
-  scriptObj.x = 20;
-}
-
-console.log(scriptObj.x);
-scopeFun();
-console.log(scriptObj.x);
-
-// passing params
-
-function init() {
-  let a = 40;
-  let b = 89;
-  let d = 90;
-  setup(a, b, d);
-}
-
-function setup(...args) {
-  console.log(args[0]);
-  console.log(args[1]);
-}
-
-init();
+let afunc = closure(3);
+afunc(9);
