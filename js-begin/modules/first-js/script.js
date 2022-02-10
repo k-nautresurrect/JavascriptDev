9b7eb526660be5fb69268f519ea87ad2b5785c04
k-nautresurrect <kushignite@gmail.com>
2022-02-10 11:34:29 +0530

    Call Stack and Stack trace

--- js-begin/modules/first-js/script.js
+++ js-begin/modules/first-js/script.js
@@ -1,33 +1,21 @@
-// Arrow function
-let sumArrow = (a, b) => a + b; // minimal operations
+// Stacktrace and call stack by chrome dev
 
-console.log(sumArrow(4, 5));
-console.log(sumArrow);
-console.log(sumArrow.toString());
-
-/**
- * arrow function also follows all the
- * properties of function hoisting
- * which states that
- *  the function is considered when interpreter read line by
- *  line code and only then the whole function will come in
- *  action
- */
-
-let printName = (name) => name; // (name) => { return name; }
-
-console.log(printName("kushagra"));
-
-let printHi = (name) => "hi " + name;
-
-console.log(printHi("kushagra"));
-
-let noParams = () => console.log("hi");
-
-noParams();
-
-function func(x, callback) {
-  callback(x);
+function doThing(a, b, name) {
+  print(sum(a, b));
+  print(editedName(name));
 }
 
-func("this", (str) => console.log(str));
+function print(variable) {
+  console.log(variable);
+}
+
+function editedName(name) {
+  // adhoaido; // Stacktrace throws error however a breakpoint can be defined.
+  return "hello " + name;
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+doThing(3, 7, "kushagra");
